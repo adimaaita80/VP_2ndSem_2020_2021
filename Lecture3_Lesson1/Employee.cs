47070cff412103ceed28a94a6f7f5fb7bfb0d3c5
DESKTOP-K9QSE2I\Adi Maaita <adi.maaita80@gmail.com>
2021-03-11 10:49:34 +0200

    Commit changes at 10:50 _ 11/3/2021

---
+++ Lecture3_Lesson1/Employee.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture3_Lesson1
+{
+    public class Employee
+    {
+        // In C# we use properties instead of plain attributes
+        // e.g. Attribute
+        //private int employeeName;
+
+        // Now use properties
+        public int EmployeeId { get; set; }
+        public string EmployeeName { get; set; }
+
+        public Employee()
+        {
+
+        }
+
+        public Employee(int empId, string empName)
+        {
+            EmployeeId = empId;
+            EmployeeName = empName;
+        }
+
+        // Now let us implement some functionality
+        public string EmployeeInformation()
+        {
+            return $"Employee ID: {EmployeeId}, Employee Name: {EmployeeName}";
+        }
+
+    }
+}
--- Lecture3_Lesson1/Program.cs
+++ Lecture3_Lesson1/Program.cs
@@ -10,6 +10,25 @@ namespace Lecture3_Lesson1
     {
         static void Main(string[] args)
         {
+            // Our lesson today is about classes and objects
+
+            // Let's create an object of type Employee
+            //Employee employee = new Employee();
+
+            //employee.EmployeeId = 1;
+            //employee.EmployeeName = "Adi Maaita";
+
+            //Console.WriteLine($"Employee ID: {employee.EmployeeId}, Employee Name: {employee.EmployeeName}");
+
+
+            Employee employee = new Employee(1, "Adi Maaita");
+
+            string summary = employee.EmployeeInformation() + "********";
+
+            Console.WriteLine(summary);
+
+            //Console.WriteLine(employee.EmployeeInformation());
+
         }
     }
 }
