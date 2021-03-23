6eb6dde948e3accaf9fe4f1f8cacc4084c056002
DESKTOP-K9QSE2I\Adi Maaita <adi.maaita80@gmail.com>
2021-03-23 10:50:51 +0200

    Lecture 8

--- Lecture7_Lesson1/Form1.Designer.cs
+++ Lecture7_Lesson1/Form1.Designer.cs
@@ -38,9 +38,9 @@ namespace Lecture7_Lesson1
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.lblNationality = new System.Windows.Forms.Label();
             this.groupBoxGender = new System.Windows.Forms.GroupBox();
-            this.lblGender = new System.Windows.Forms.Label();
-            this.rbMale = new System.Windows.Forms.RadioButton();
             this.rbFemale = new System.Windows.Forms.RadioButton();
+            this.rbMale = new System.Windows.Forms.RadioButton();
+            this.lblGender = new System.Windows.Forms.Label();
             this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
             this.lblDateOfBirth = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
@@ -48,6 +48,12 @@ namespace Lecture7_Lesson1
             this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
             this.label2 = new System.Windows.Forms.Label();
             this.btnSubmit = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.lblBloodType = new System.Windows.Forms.Label();
+            this.lblPhoneNumber = new System.Windows.Forms.Label();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.maskedTextBox2 = new System.Windows.Forms.MaskedTextBox();
             this.groupBoxGender.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -56,46 +62,46 @@ namespace Lecture7_Lesson1
             this.lblFirstName.AutoSize = true;
             this.lblFirstName.Location = new System.Drawing.Point(64, 51);
             this.lblFirstName.Name = "lblFirstName";
-            this.lblFirstName.Size = new System.Drawing.Size(57, 13);
+            this.lblFirstName.Size = new System.Drawing.Size(97, 18);
             this.lblFirstName.TabIndex = 0;
             this.lblFirstName.Text = "First Name";
             // 
             // txtFirstName
             // 
-            this.txtFirstName.Location = new System.Drawing.Point(139, 48);
+            this.txtFirstName.Location = new System.Drawing.Point(164, 48);
             this.txtFirstName.Name = "txtFirstName";
-            this.txtFirstName.Size = new System.Drawing.Size(202, 20);
+            this.txtFirstName.Size = new System.Drawing.Size(202, 27);
             this.txtFirstName.TabIndex = 1;
             // 
             // txtMiddleName
             // 
-            this.txtMiddleName.Location = new System.Drawing.Point(442, 48);
+            this.txtMiddleName.Location = new System.Drawing.Point(524, 48);
             this.txtMiddleName.Name = "txtMiddleName";
-            this.txtMiddleName.Size = new System.Drawing.Size(202, 20);
+            this.txtMiddleName.Size = new System.Drawing.Size(202, 27);
             this.txtMiddleName.TabIndex = 3;
             // 
             // lblMiddleName
             // 
             this.lblMiddleName.AutoSize = true;
-            this.lblMiddleName.Location = new System.Drawing.Point(367, 51);
+            this.lblMiddleName.Location = new System.Drawing.Point(407, 51);
             this.lblMiddleName.Name = "lblMiddleName";
-            this.lblMiddleName.Size = new System.Drawing.Size(69, 13);
+            this.lblMiddleName.Size = new System.Drawing.Size(114, 18);
             this.lblMiddleName.TabIndex = 2;
             this.lblMiddleName.Text = "Middle Name";
             // 
             // txtLastName
             // 
-            this.txtLastName.Location = new System.Drawing.Point(752, 48);
+            this.txtLastName.Location = new System.Drawing.Point(871, 48);
             this.txtLastName.Name = "txtLastName";
-            this.txtLastName.Size = new System.Drawing.Size(202, 20);
+            this.txtLastName.Size = new System.Drawing.Size(202, 27);
             this.txtLastName.TabIndex = 5;
             // 
             // lblLast
             // 
             this.lblLast.AutoSize = true;
-            this.lblLast.Location = new System.Drawing.Point(677, 51);
+            this.lblLast.Location = new System.Drawing.Point(769, 51);
             this.lblLast.Name = "lblLast";
-            this.lblLast.Size = new System.Drawing.Size(58, 13);
+            this.lblLast.Size = new System.Drawing.Size(96, 18);
             this.lblLast.TabIndex = 4;
             this.lblLast.Text = "Last Name";
             // 
@@ -108,17 +114,17 @@ namespace Lecture7_Lesson1
             "Iraq",
             "Syria",
             "Egypt"});
-            this.comboBox1.Location = new System.Drawing.Point(657, 215);
+            this.comboBox1.Location = new System.Drawing.Point(164, 224);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(297, 21);
-            this.comboBox1.TabIndex = 6;
+            this.comboBox1.Size = new System.Drawing.Size(297, 26);
+            this.comboBox1.TabIndex = 8;
             // 
             // lblNationality
             // 
             this.lblNationality.AutoSize = true;
-            this.lblNationality.Location = new System.Drawing.Point(588, 218);
+            this.lblNationality.Location = new System.Drawing.Point(53, 220);
             this.lblNationality.Name = "lblNationality";
-            this.lblNationality.Size = new System.Drawing.Size(56, 13);
+            this.lblNationality.Size = new System.Drawing.Size(98, 18);
             this.lblNationality.TabIndex = 7;
             this.lblNationality.Text = "Nationality";
             // 
@@ -126,65 +132,65 @@ namespace Lecture7_Lesson1
             // 
             this.groupBoxGender.Controls.Add(this.rbFemale);
             this.groupBoxGender.Controls.Add(this.rbMale);
-            this.groupBoxGender.Location = new System.Drawing.Point(139, 215);
+            this.groupBoxGender.Location = new System.Drawing.Point(164, 270);
             this.groupBoxGender.Name = "groupBoxGender";
             this.groupBoxGender.Size = new System.Drawing.Size(297, 137);
-            this.groupBoxGender.TabIndex = 8;
+            this.groupBoxGender.TabIndex = 9;
             this.groupBoxGender.TabStop = false;
-            // 
-            // lblGender
-            // 
-            this.lblGender.AutoSize = true;
-            this.lblGender.Location = new System.Drawing.Point(65, 224);
-            this.lblGender.Name = "lblGender";
-            this.lblGender.Size = new System.Drawing.Size(42, 13);
-            this.lblGender.TabIndex = 9;
-            this.lblGender.Text = "Gender";
-            // 
-            // rbMale
-            // 
-            this.rbMale.AutoSize = true;
-            this.rbMale.Location = new System.Drawing.Point(25, 20);
-            this.rbMale.Name = "rbMale";
-            this.rbMale.Size = new System.Drawing.Size(48, 17);
-            this.rbMale.TabIndex = 0;
-            this.rbMale.TabStop = true;
-            this.rbMale.Text = "Male";
-            this.rbMale.UseVisualStyleBackColor = true;
             // 
             // rbFemale
             // 
             this.rbFemale.AutoSize = true;
             this.rbFemale.Location = new System.Drawing.Point(25, 78);
             this.rbFemale.Name = "rbFemale";
-            this.rbFemale.Size = new System.Drawing.Size(59, 17);
+            this.rbFemale.Size = new System.Drawing.Size(85, 22);
             this.rbFemale.TabIndex = 1;
             this.rbFemale.TabStop = true;
             this.rbFemale.Text = "Female";
             this.rbFemale.UseVisualStyleBackColor = true;
             // 
+            // rbMale
+            // 
+            this.rbMale.AutoSize = true;
+            this.rbMale.Location = new System.Drawing.Point(25, 20);
+            this.rbMale.Name = "rbMale";
+            this.rbMale.Size = new System.Drawing.Size(64, 22);
+            this.rbMale.TabIndex = 0;
+            this.rbMale.TabStop = true;
+            this.rbMale.Text = "Male";
+            this.rbMale.UseVisualStyleBackColor = true;
+            // 
+            // lblGender
+            // 
+            this.lblGender.AutoSize = true;
+            this.lblGender.Location = new System.Drawing.Point(65, 259);
+            this.lblGender.Name = "lblGender";
+            this.lblGender.Size = new System.Drawing.Size(66, 18);
+            this.lblGender.TabIndex = 9;
+            this.lblGender.Text = "Gender";
+            // 
             // dateTimePicker1
             // 
-            this.dateTimePicker1.Location = new System.Drawing.Point(139, 131);
+            this.dateTimePicker1.Location = new System.Drawing.Point(164, 131);
             this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(297, 20);
-            this.dateTimePicker1.TabIndex = 10;
+            this.dateTimePicker1.Size = new System.Drawing.Size(297, 27);
+            this.dateTimePicker1.TabIndex = 6;
             // 
             // lblDateOfBirth
             // 
             this.lblDateOfBirth.AutoSize = true;
-            this.lblDateOfBirth.Location = new System.Drawing.Point(55, 137);
+            this.lblDateOfBirth.Location = new System.Drawing.Point(44, 137);
             this.lblDateOfBirth.Name = "lblDateOfBirth";
-            this.lblDateOfBirth.Size = new System.Drawing.Size(66, 13);
+            this.lblDateOfBirth.Size = new System.Drawing.Size(114, 18);
             this.lblDateOfBirth.TabIndex = 11;
             this.lblDateOfBirth.Text = "Date of Birth";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(583, 137);
+            this.label1.Location = new System.Drawing.Point(40, 179);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(70, 13);
+            this.label1.Size = new System.Drawing.Size(118, 18);
             this.label1.TabIndex = 13;
             this.label1.Text = "Place of Birth";
             // 
@@ -197,10 +203,10 @@ namespace Lecture7_Lesson1
             "Iraq",
             "Syria",
             "Egypt"});
-            this.comboBox2.Location = new System.Drawing.Point(657, 134);
+            this.comboBox2.Location = new System.Drawing.Point(164, 178);
             this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(297, 21);
-            this.comboBox2.TabIndex = 12;
+            this.comboBox2.Size = new System.Drawing.Size(297, 26);
+            this.comboBox2.TabIndex = 7;
             // 
             // checkedListBox1
             // 
@@ -209,35 +215,101 @@ namespace Lecture7_Lesson1
             "Non-steroidal",
             "Milk",
             "Lactose"});
-            this.checkedListBox1.Location = new System.Drawing.Point(139, 393);
+            this.checkedListBox1.Location = new System.Drawing.Point(164, 427);
             this.checkedListBox1.Name = "checkedListBox1";
-            this.checkedListBox1.Size = new System.Drawing.Size(297, 49);
-            this.checkedListBox1.TabIndex = 14;
+            this.checkedListBox1.Size = new System.Drawing.Size(297, 70);
+            this.checkedListBox1.TabIndex = 10;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(72, 393);
+            this.label2.Location = new System.Drawing.Point(72, 432);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(46, 13);
+            this.label2.Size = new System.Drawing.Size(79, 18);
             this.label2.TabIndex = 15;
             this.label2.Text = "Allergies";
             // 
             // btnSubmit
             // 
-            this.btnSubmit.Location = new System.Drawing.Point(139, 481);
+            this.btnSubmit.BackColor = System.Drawing.Color.Navy;
+            this.btnSubmit.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSubmit.ForeColor = System.Drawing.Color.White;
+            this.btnSubmit.Location = new System.Drawing.Point(164, 624);
             this.btnSubmit.Name = "btnSubmit";
             this.btnSubmit.Size = new System.Drawing.Size(148, 48);
-            this.btnSubmit.TabIndex = 16;
+            this.btnSubmit.TabIndex = 12;
             this.btnSubmit.Text = "submit";
-            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.UseVisualStyleBackColor = false;
             this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 18;
+            this.listBox1.Items.AddRange(new object[] {
+            "A+",
+            "O+",
+            "O-",
+            "AB"});
+            this.listBox1.Location = new System.Drawing.Point(164, 523);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(73, 76);
+            this.listBox1.TabIndex = 11;
+            // 
+            // lblBloodType
+            // 
+            this.lblBloodType.AutoSize = true;
+            this.lblBloodType.Location = new System.Drawing.Point(61, 523);
+            this.lblBloodType.Name = "lblBloodType";
+            this.lblBloodType.Size = new System.Drawing.Size(97, 18);
+            this.lblBloodType.TabIndex = 18;
+            this.lblBloodType.Text = "Blood Type";
+            // 
+            // lblPhoneNumber
+            // 
+            this.lblPhoneNumber.AutoSize = true;
+            this.lblPhoneNumber.Location = new System.Drawing.Point(521, 137);
+            this.lblPhoneNumber.Name = "lblPhoneNumber";
+            this.lblPhoneNumber.Size = new System.Drawing.Size(127, 18);
+            this.lblPhoneNumber.TabIndex = 19;
+            this.lblPhoneNumber.Text = "Phone Number";
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Location = new System.Drawing.Point(595, 179);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(53, 18);
+            this.lblEmail.TabIndex = 20;
+            this.lblEmail.Text = "Email";
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(675, 131);
+            this.maskedTextBox1.Mask = "(99) 0000000";
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(202, 27);
+            this.maskedTextBox1.TabIndex = 21;
+            // 
+            // maskedTextBox2
+            // 
+            this.maskedTextBox2.Location = new System.Drawing.Point(675, 176);
+            this.maskedTextBox2.Name = "maskedTextBox2";
+            this.maskedTextBox2.Size = new System.Drawing.Size(398, 27);
+            this.maskedTextBox2.TabIndex = 22;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1132, 642);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1123, 713);
+            this.Controls.Add(this.maskedTextBox2);
+            this.Controls.Add(this.maskedTextBox1);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.lblPhoneNumber);
+            this.Controls.Add(this.lblBloodType);
+            this.Controls.Add(this.listBox1);
             this.Controls.Add(this.btnSubmit);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.checkedListBox1);
@@ -255,7 +327,10 @@ namespace Lecture7_Lesson1
             this.Controls.Add(this.lblMiddleName);
             this.Controls.Add(this.txtFirstName);
             this.Controls.Add(this.lblFirstName);
+            this.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Baby Registration Form";
             this.groupBoxGender.ResumeLayout(false);
             this.groupBoxGender.PerformLayout();
@@ -285,6 +360,12 @@ namespace Lecture7_Lesson1
         private System.Windows.Forms.CheckedListBox checkedListBox1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label lblBloodType;
+        private System.Windows.Forms.Label lblPhoneNumber;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox2;
     }
 }
 
